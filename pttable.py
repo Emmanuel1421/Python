15cb1674431e4c17ded5f40c210ebd9f76d67d99
Emmanuel Guerra <emmanuelguerra1945@gmail.com>
2025-03-21 11:40:46 -0300

    Update pttable.py
    a

--- pttable.py
+++ pttable.py
@@ -1,5 +1,6 @@
 '''from prettytable import PrettyTable
 import json
+import csv
 
 data = {
     "name": "João",
@@ -20,7 +21,7 @@ table.align["Nome"]= "l"
 table.align["Idade"]= "r"
 print(table)  '''
 
-
+'''
 valor1 = "{:<8}".format("123")
 print(valor1)
 
@@ -31,4 +32,27 @@ valor3 = "{:^5}".format("CDE")
 print(valor3)
 
 valor4 = "{:6.2f}".format(12.456)
-print(valor4)
+print(valor4)'''
+'''import sys
+from datetime import datetime
+
+nome = "Jailson"
+idade = 25
+data_nascimento = datetime(1998, 5, 12)
+
+separador = " | "
+
+finalizador = " - "
+
+print("Nome:", nome, sep=separador, end=finalizador)
+print("Idade:", idade, sep=separador, end=finalizador)
+print("Data de Nascimento:", data_nascimento.strftime("%d/%m/%Y"),file=sys.stderr, flush=True)
+
+
+print ("O seu nome é: ",nome)
+'''
+'''
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite a sua idade: "))
+numero = float(input("Digite um número: "))
+print(f"Olá, {nome}! Você tem {idade} anos e o número que você digitou foi {numero:.2f}")'''
